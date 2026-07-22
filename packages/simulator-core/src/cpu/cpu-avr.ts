96eafbbc0d1ce0c15135392247339c1725bfb9b6
MoDevIO <momo.tiltis@gmail.com>
2026-07-22 17:08:52 +0200

    feat: sync AVR-GPIO with simulator-GPIO

--- packages/simulator-core/src/cpu/cpu-avr.ts
+++ packages/simulator-core/src/cpu/cpu-avr.ts
@@ -1,14 +1,39 @@
-import { CPU as AVRCore, avrInstruction } from "avr8js";
+import {
+  CPU as AVRCore,
+  avrInstruction,
+  AVRIOPort,
+  PinState as AVRPinState,
+  portBConfig,
+  portCConfig,
+  portDConfig,
+} from "avr8js";
 
 import type { CPU } from "./cpu.js";
 import { GPIO } from "../gpio/gpio.js";
 import { Port } from "../gpio/port.js";
+import { PinState } from "../gpio/state.js";
 
 export class AVRCPU implements CPU {
   private cpu: AVRCore;
   private flash: Uint16Array;
 
   readonly gpio: GPIO;
+
+  private portB: AVRIOPort;
+  private portC: AVRIOPort;
+  private portD: AVRIOPort;
+
+  private bindPort(avrPort: AVRIOPort, gpioPort: Port): void {
+    avrPort.addListener(() => {
+      for (const pin of gpioPort.pins) {
+        const state =
+          avrPort.pinState(pin.number) === AVRPinState.High
+            ? PinState.High
+            : PinState.Low;
+        pin.setState(state);
+      }
+    });
+  }
 
   constructor() {
     const FLASH_SIZE = (32 * 1024) / 2; // 32KB flash size in words (16 bit)
@@ -22,6 +47,25 @@ export class AVRCPU implements CPU {
       new Port("PortC", 7),
       new Port("PortD", 8),
     ]);
+
+    this.portB = new AVRIOPort(this.cpu, portBConfig);
+    this.portC = new AVRIOPort(this.cpu, portCConfig);
+    this.portD = new AVRIOPort(this.cpu, portDConfig);
+
+    // Connect GPIO ports to AVR ports
+    // this.portB.addListener(() => {
+    //   const pin5 = this.gpio.getPort("PortB").getPin(5);
+    //   const pin5State =
+    //     this.portB.pinState(5) === AVRPinState.High
+    //       ? PinState.High
+    //       : PinState.Low;
+    //
+    //   pin5.setState(pin5State);
+    // });
+
+    this.bindPort(this.portB, this.gpio.getPort("PortB"));
+    this.bindPort(this.portC, this.gpio.getPort("PortC"));
+    this.bindPort(this.portD, this.gpio.getPort("PortD"));
   }
 
   loadFirmware(firmware: Uint8Array): void {
---
+++ packages/simulator-core/src/gpio/state.ts
@@ -0,0 +1,10 @@
+export enum PinState {
+  Low = 0,
+  High = 1,
+}
+
+export enum PinMode {
+  Input,
+  Output,
+  InputPullup,
+}
